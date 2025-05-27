deee9fc7d7f12b983e0a7803cc0c04f7e96ef02d
chauveau <chauveau@cenbg.in2p3.fr>
2025-05-27 14:38:13 +0200

    rename + rearrange killer module

--- cd-killer-module.cc
+++ cd-killer-module.cc
@@ -1,89 +1,96 @@
 #include <bayeux/dpp/chain_module.h>
-#include <bayeux/genbb_help/primary_event.h>
-#include <bayeux/mctools/base_step_hit.h>
-#include <bayeux/mctools/simulated_data.h>
 
 #include <falaise/snemo/datamodels/event_header.h>
 #include <falaise/snemo/datamodels/geomid_utils.h>
 #include <falaise/snemo/datamodels/calibrated_data.h>
 
-class cd_killer : public dpp::chain_module
+#include <cstdio>
+
+////////////////////////////////////////////////////////////////
+
+class cd_killer_module : public dpp::chain_module
 {
 public:
-  cd_killer();
-  virtual ~cd_killer();
-  virtual void initialize(const datatools::properties &myConfig,
-                          datatools::service_manager& flServices,
-                          dpp::module_handle_dict_type& moduleDict);
+  cd_killer_module();
+  ~cd_killer_module();
 
-  virtual dpp::chain_module::process_status process(datatools::things &event);
+  void initialize(const datatools::properties &,
+		  datatools::service_manager &,
+		  dpp::module_handle_dict_type &);
   
-  virtual void finalize ();
+  dpp::chain_module::process_status process(datatools::things &);
+
+  void finalize();
 
 private:
-  DPP_MODULE_REGISTRATION_INTERFACE(cd_killer);
-
   bool calo_dead_om[712];
   bool tracker_dead_gg[2034];
 
   float calo_energy_threshold;
+
+  DPP_MODULE_REGISTRATION_INTERFACE(cd_killer_module);
 };
 
-DPP_MODULE_REGISTRATION_IMPLEMENT(cd_killer, "cd_killer");
+DPP_MODULE_REGISTRATION_IMPLEMENT(cd_killer_module, "cd_killer_module");
 
-cd_killer::cd_killer() : dpp::chain_module() {}
+////////////////////////////////////////////////////////////////
 
-cd_killer::~cd_killer()
+cd_killer_module::cd_killer_module() : dpp::chain_module()
+{
+  
+}
+
+cd_killer_module::~cd_killer_module()
 {
   this->finalize();
 }
 
-void cd_killer::initialize(const datatools::properties  &myConfig,
-			      datatools::service_manager   &/*&flServices*/,
-			      dpp::module_handle_dict_type &/*moduleDict*/)
+void cd_killer_module::initialize(const datatools::properties & config,
+			   datatools::service_manager & ,
+			   dpp::module_handle_dict_type & )
 {
-  if (myConfig.has_key("calo.dead_om"))
+  if (config.has_key("calo.dead_om"))
     {
       std::vector<int> dead_om_num_v;
-      myConfig.fetch("calo.dead_om", dead_om_num_v);
+      config.fetch("calo.dead_om", dead_om_num_v);
 
       memset(calo_dead_om, false, 712*sizeof(bool));
 
       printf("-> %zd dead OM: ", dead_om_num_v.size());
 
-      for (int dead_om_num : dead_om_num_v) {
+      for (const int dead_om_num : dead_om_num_v) {
 	calo_dead_om[dead_om_num] = true;
 	printf("%d ", dead_om_num);}
 
       printf("\n");
     }
 
-  if (myConfig.has_key("tracker.dead_gg"))
+  if (config.has_key("tracker.dead_gg"))
     {
       std::vector<int> dead_cell_num_v;
-      myConfig.fetch("tracker.dead_gg", dead_cell_num_v);
+      config.fetch("tracker.dead_gg", dead_cell_num_v);
 
       memset(tracker_dead_gg, false, 2034*sizeof(bool));
 
       printf("-> %zd dead CELL: ", dead_cell_num_v.size());
 
-      for (int dead_cell_num : dead_cell_num_v) {
+      for (const int dead_cell_num : dead_cell_num_v) {
 	tracker_dead_gg[dead_cell_num] = true;
 	printf("%d ", dead_cell_num);}
 
       printf("\n");
     }
 
-  if (myConfig.has_key("calo.energy_threshold"))
+  if (config.has_key("calo.energy_threshold"))
     {
-      calo_energy_threshold = myConfig.fetch_real("calo.energy_threshold");
+      calo_energy_threshold = config.fetch_real("calo.energy_threshold");
       printf("-> calo energy threshold: %.1f MeV\n", calo_energy_threshold);
     }
 
   this->_set_initialized(true);
 }
 
-dpp::chain_module::process_status cd_killer::process(datatools::things &event)
+dpp::chain_module::process_status cd_killer_module::process(datatools::things &event)
 {
   snemo::datamodel::calibrated_data & CD = event.grab<snemo::datamodel::calibrated_data>("CD");
 
@@ -120,7 +127,7 @@ dpp::chain_module::process_status cd_killer::process(datatools::things &event)
   return PROCESS_SUCCESS;
 }
 
-void cd_killer::finalize()
+void cd_killer_module::finalize()
 {
   this->_set_initialized(false);
 }
